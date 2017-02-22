0401a655ade068722a024e44193a3178f2c69aad
ivanpsl <ivanpsl95@gmail.com>
2017-02-22 18:06:04 +0100

    CambiarEmail, solucion errores

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -7,7 +7,6 @@ import model.UserInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +15,7 @@ import service.UserService;
 import service.UserServiceImpl;
 import util.Encriptador;
 import util.ParticipantsException;
+import util.VerificadorEmail;
 
 @Controller
 public class MainController {
@@ -44,7 +44,7 @@ public class MainController {
 	public String testUsuario(Model model) {
 		@SuppressWarnings("deprecation")
 		Date fecha = new Date(0, 0, 0);
-		model.addAttribute("user", new UserInfo("testUser", Encriptador.encriptar("ss"), "email@tes.com", "123T", 
+		model.addAttribute("user", new UserInfo("testUser", Encriptador.encriptar("ss"), "email@test.com", "123T", 
 				"TestName", "TestApp",fecha , "C/test", "España"));
 	
 		return "infoUsuario";
@@ -71,15 +71,55 @@ public class MainController {
 	}
 	
 	@RequestMapping(value = "/volverAinfo", 
-			method = RequestMethod.POST)
+			method = RequestMethod.GET)
 	public String getParticipantInfo(Model modelo,@ModelAttribute("user") UserInfo usuario) {
 		modelo.addAttribute("user", usuario);
 		return "infoUsuario";
 	}
 	
-	@RequestMapping(value = "/cambiar")
-	public String navegarCambiarContrasena(Model modelo)
+	@RequestMapping(value = "/cambiarE")
+	public String navegarCambiarEmail(Model modelo)
 	{
+		modelo.addAttribute("err", " ");
+		return "cambiarEmail";
+	}
+
+
+	
+	@RequestMapping(value = "/cambioEmail", 
+			method = RequestMethod.POST)
+	public String changeEmail(Model modelo,@ModelAttribute("user") UserInfo usuario, @RequestParam String password,
+			@RequestParam String newEmail){
+		userService = new UserServiceImpl();
+		try {
+			
+			if(!password.equals(Encriptador.Desencriptar(usuario.getPassword()))){
+				modelo.addAttribute("err", "Contraseña incorrecta");
+				return "cambiarEmail";
+			}
+			if(!VerificadorEmail.validateEmail(newEmail)) {
+				modelo.addAttribute("err", "Email incorrecto");
+				return "cambiarEmail";
+			}
+			
+			modelo.addAttribute("err", "");
+			userService.changeEmail(usuario, newEmail);
+			modelo.addAttribute("err","");
+			modelo.addAttribute("user", usuario);
+			modelo.addAttribute("success","Se ha actualizado la contraseña correctamente");
+			return "exito";
+	
+		} catch (Exception e) {
+			e.printStackTrace();
+			modelo.addAttribute("err",e.getMessage());
+			return "error";
+		}
+	}
+	
+	@RequestMapping(value = "/cambiar")
+	public String navegarCambiarContrasena(Model modelo, @ModelAttribute("user") UserInfo usuario)
+	{
+		modelo.addAttribute("user",usuario);
 		modelo.addAttribute("err", " ");
 		return "cambiarPass";
 	}
@@ -93,7 +133,7 @@ public class MainController {
 		try {
 			
 			if(!password.equals(Encriptador.Desencriptar(usuario.getPassword()))){
-				modelo.addAttribute("err", "Contraseña no incorrecta");
+				modelo.addAttribute("err", "Contraseña incorrecta");
 				return "cambiarPass";
 			}
 			if(newPassword1.length()<=0) {
